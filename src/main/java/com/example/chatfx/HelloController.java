19d48dc2086e5d6add003b38d6e39576e35c4a6d
VADUMr <115543904+VADUMr@users.noreply.github.com>
2024-04-23 22:30:30 +0300

    Finish chat

--- src/main/java/com/example/chatfx/HelloController.java
+++ src/main/java/com/example/chatfx/HelloController.java
@@ -1,14 +1,86 @@
 package com.example.chatfx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.application.Platform;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class HelloController{
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField ServerIP;
+    @FXML
+    private TextField ChatNickname;
+    @FXML
+    private TextField Message;
+    @FXML
+    private TextArea messageArea;
+
+    @FXML
+    protected void connect() {
+        try {
+            String IP = ServerIP.getText();
+            socket = new Socket(IP, 8888);
+            Platform.runLater(() -> messageArea.appendText("Підключено до сервера." + "\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setupStreams();
+        startReceivingMessages();
+    }
+    private void setupStreams() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+
+            String nickname = ChatNickname.getText();
+            writer.println(nickname);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void startReceivingMessages() {
+        Thread receiveThread = new Thread(() -> {
+            try {
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    String finalMessage = message;
+                    Platform.runLater(() -> messageArea.appendText(finalMessage + "\n"));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        receiveThread.start();
+    }
+    @FXML
+    protected void sendMessage() {
+        String message = Message.getText().trim();
+        if (!message.isEmpty()) {
+            writer.println(message);
+            Message.clear();
+        }
+    }
+    @FXML
+    protected void disconnect() {
+        try {
+            if (socket != null) {
+                socket.close();
+                Platform.runLater(() -> messageArea.appendText("Відключено від сервера." + "\n"));
+            }
+            //Platform.exit(); // Закриття вікна програми
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
